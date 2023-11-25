26d2725c29159889c9c334401acc5ac2fba80d2f
sakurakotubaki <gitonly543@gmail.com>
2023-11-25 16:54:53 +0900

    wip

---
+++ TodoDemo/Component/TaskRow.swift
@@ -0,0 +1,20 @@
+//
+//  TaskRow.swift
+//  TodoDemo
+//
+//  Created by 橋本純一 on 2023/11/25.
+//
+
+import SwiftUI
+
+struct TaskRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskRow()
+    }
+}
---
+++ TodoDemo/TasksIView.swift
@@ -0,0 +1,20 @@
+//
+//  TasksIView.swift
+//  TodoDemo
+//
+//  Created by 橋本純一 on 2023/11/25.
+//
+
+import SwiftUI
+
+struct TasksIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TasksIView_Previews: PreviewProvider {
+    static var previews: some View {
+        TasksIView()
+    }
+}
