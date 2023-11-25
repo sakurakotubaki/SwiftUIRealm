b5619ed840b8e0824e1636581e205f4245a446f4
sakurakotubaki <gitonly543@gmail.com>
2023-11-25 15:25:49 +0900

    Initial Commit

---
+++ TodoDemo/TodoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoDemoApp.swift
+//  TodoDemo
+//
+//  Created by 橋本純一 on 2023/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
